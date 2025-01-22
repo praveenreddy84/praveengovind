da4461eaaf0125ee4dfdaef49db50dca2555bd69
Praveen Govind <govindpraveen@gmail.com>
2025-01-21 20:14:49 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Praveen Govind test.</li>
+          <li>Praveen Govind test2.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
